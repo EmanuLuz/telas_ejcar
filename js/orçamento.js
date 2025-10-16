52881997c7b44e18aef921d0907a6d7a5f426f76
EmanuLuz <emanueli.bessa@escola.pr.gov.br>
2025-10-16 08:20:03 -0300

    arrumei a tela vistoria e coloquei novas telas

--- js/orçamento.js
+++ js/orçamento.js
@@ -1,14 +1,26 @@
-        document.addEventListener('DOMContentLoaded', function() {
-            const addProductBtn = document.getElementById('add-product-btn');
-           
-            addProductBtn.addEventListener('click', function() {
-                alert('Funcionalidade "Mais Produtos" seria implementada aqui!');
-              
-            });
-           
-         
-            document.getElementById('budget-form').addEventListener('submit', function(e) {
-                e.preventDefault();
-                alert('Orçamento finalizado com sucesso!');
-            });
-        });
+function finalizar(){
+    const chassi = document.getElementById("chassi").value;
+
+    const placa = document.getElementById("placa").value;
+    
+    const produto = document.getElementById("produto").value;
+
+    const mao_de_obra = document.getElementById("mao_de_obra").value;
+    alert(chassi + " - " + placa + " - " + produto + " - " + mao_de_obra);
+
+        if(chassi == ""){
+                alert("Você precisa preencher o campo chassi");
+        }
+
+        if(placa == ""){
+                alert("Você precisa preencher o campo placa");
+        }
+
+        if(produto == ""){
+                alert("Você precisa preencher o campo produto");
+        }
+     
+        if(mao_de_obra == ""){
+                alert("Você precisa preencher o campo mao de obra");
+        }
+}
